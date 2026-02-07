7259bce2b018f7d6be44e05fa7bf4d6a492b7407
FrancoisCharat <francois.charat@gmail.com>
2026-02-07 01:19:55 +0100

    Update BuildingV5.js

--- smart-building/BuildingV5.js
+++ smart-building/BuildingV5.js
@@ -250,7 +250,7 @@ if (reversed == null) { reversed = false; }
 	this.shape.setTransform(299.25,53.95);
 
 	this.shape_1 = new cjs.Shape();
-	this.shape_1.graphics.f("#FFFFFF").s().p("AgeBTQgPgGgIgMQgJgNABgPIAOAAQAAASAOALQAOALAWAAQAUAAANgJQAMgJAAgPQAAgOgKgJQgLgIgagIQgbgGgMgKQgSgMAAgVQAAgUAQgMQAQgNAaAAQARAAAOAHQAOAHAHALQAIAMAAAPIgPAAQgBgRgLgMQgNgKgUAAQgUAAgLAJQgMAIAAAPQAAANAKAJQALAHAXAHQAaAIAKAFQAMAHAFAKQAGAIABAOQAAAVgRAMQgRAMgaAAQgRABgQgHg");
+	this.shape_1.graphics.f("#FFCE00").s().p("AgeBTQgPgGgIgMQgJgNABgPIAOAAQAAASAOALQAOALAWAAQAUAAANgJQAMgJAAgPQAAgOgKgJQgLgIgagIQgbgGgMgKQgSgMAAgVQAAgUAQgMQAQgNAaAAQARAAAOAHQAOAHAHALQAIAMAAAPIgPAAQgBgRgLgMQgNgKgUAAQgUAAgLAJQgMAIAAAPQAAANAKAJQALAHAXAHQAaAIAKAFQAMAHAFAKQAGAIABAOQAAAVgRAMQgRAMgaAAQgRABgQgHg");
 	this.shape_1.setTransform(463.25,71);
 
 	this.shape_2 = new cjs.Shape();
@@ -10767,6 +10767,7 @@ an.handleSoundStreamOnTick = function(event) {
 })(createjs = createjs||{}, AdobeAn = AdobeAn||{});
 
 var createjs, AdobeAn;
+
 
 
 
